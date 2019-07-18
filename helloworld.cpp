a19ae65ae84bcf299987b868a000cba6af9d0997
root <root@localhost.localdomain>
2019-07-18 12:35:50 +0900

    addressbook

---
+++ helloworld.cpp
@@ -0,0 +1,29 @@
+#include <eosiolib/eosio.hpp>
+
+using namespace eosio;
+
+CONTRACT helloworld: public contract {
+    public: 
+        
+        using contract::contract;
+            ACTION hi(name user) {
+            // bool isTrue = has_auth(get_self()) || has_auth("hahahoho1234"_n);
+            // check(isTrue, "you aint a contract deployer!!")
+            // check(has_auth(get_self()) || has_auth("hahahoho1234"_n),"missing authority");
+            //check(has_auth("moonmoon1234"_n), "missing authority"); // has_auth() -> true or false 
+            
+            // == require_auth(user);
+            // require_auth("moonmoon1234"_n);
+            // require_auth(get_self()); -> 동적으로,, 할당해서 제일 추천
+            // check(has_auth(get_self(), "missing authority"));
+            
+                print("hello, ", user);
+            }
+        ACTION getaccount(name user){
+            require_auth(get_self());
+            print(is_account(user));
+        }
+    private :
+
+
+};
